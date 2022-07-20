1555d96a7f54e602265924be6709c5266387d246
Gustavo S Frehse <gusfrehse@gmail.com>
2022-07-19 21:05:54 -0300

    update

--- template.cpp
+++ template.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
 }
